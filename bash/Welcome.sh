50f0dfb28ba474c91675c41bd2eca5cffecbf6e8
Christopher <you@yourdomain>
2018-09-20 10:48:05 -0400

    Saved

---
+++ bash/Welcome.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#a script to issue a name with a title
+
+################################
+#       Variables             #
+################################
+export MYNAME="Christopher Jackson "
+mytitle="The Supreme Overlord "
+hostname=$(hostname) #poor choice of variable name, same as something else
+today=$(date +%A) #Try to always place all your variables at the start of the script together
+
+
+##################
+#      Main      #
+##################
+echo "welcome to planet $mytitle $MYNAME $hostname, $mytitle $MYNAME"
+echo "Today is $(today)."
