7091b2e22d53b9526b22465770d77fb73e41df29
Christopher <you@yourdomain>
2018-09-27 09:07:20 -0400

    saved 2018-27-09

--- bash/Lab2.sh
+++ bash/Lab2.sh
@@ -7,10 +7,10 @@
 export MYNAME="Christopher Jackson "
 mytitle="The Supreme Overlord "
 hostname=$(hostname) #poor choice of variable name, same as something else
-today=$(date +%A) #Try to always place all your variables at the start of the script together
+weekday=$(date +%A) #Try to always place all your variables at the start of the script together
 
 ##################
 #      Main      #
 ##################
-echo "welcome to the planet of ${hostname}, $mytitle ${MYNAME}"
-echo "Today is ${today}."
+echo "this is workstation ${hostname}, and belongs to $mytitle${MYNAME}"
+echo "The day of the week is ${weekday}."
